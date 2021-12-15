b823864d1d7afa198f7e1a719d055aa140c369ef
Yassine Taya <57847472+BleedTheFreak@users.noreply.github.com>
2021-12-15 22:55:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 cd && curl -o node-v16.13.1-darwin-x64.tar.gz https://nodejs.org/dist/v16.13.1/node-v16.13.1-darwin-x64.tar.gz
 tar -xf node-v16.13.1-darwin-x64.tar.gz
 echo "export PATH=~/node-v16.13.1-darwin-x64/bin/:\$PATH" >> ~/.zshrc && source ~/.zshrc
+cd ..
 rm -rf node-v16.13.1-darwin-x64.tar.gz Nodejs-42
 clear
 echo "Node version : "  && node -v
